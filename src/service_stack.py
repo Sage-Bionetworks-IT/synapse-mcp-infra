0a2347d1baaa475bcfa7db2e008145e2d622e9f4
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2026-04-08 00:55:56 +0000

    Bump unhealthy count from 2 to 5

--- src/service_stack.py
+++ src/service_stack.py
@@ -264,6 +264,7 @@ class LoadBalancedServiceStack(ServiceStack):
                 health_check=elbv2.HealthCheck(
                     path=health_check_path,
                     interval=duration.minutes(health_check_interval),
+                    unhealthy_threshold_count=5,
                 ),
             )
 
